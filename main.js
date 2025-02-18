77b8f840532f9a95584692496aed3ac9c7f996dc
shortcoin2025 <shortcoin2025@gmail.com>
2025-02-18 18:10:52 +0000

    array

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if (typeof ThreeWP !== 'undefined') {
         // Destructure THREE and THREE_ADDONS from ThreeWP
         const { THREE, OrbitControls,OBJLoader,DRACOLoader,GLTFLoader } = ThreeWP;
+        const objects = [];
         console.log("ThreeWP",ThreeWP)
         // Create a scene
         const scene = new THREE.Scene();
